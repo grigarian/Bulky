2c438262b22d0e74eb15058ba283c040787c7945
grigarian <grigarian24@mail.ru>
2024-03-04 18:20:13 +0500

    Структура проекта измена, другой дизайн

---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage ="Максимальная длинна название не должна привышать 30 символов")]
+        [DisplayName("Название категории")]
+        public string Name { get; set; }
+        [DisplayName("Порядок отображения")]
+        [Range(1,100, ErrorMessage ="Диапозон значени должен быть между 1 и 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
